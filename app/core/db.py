883189a7b4457e389776493b112d8046e7d2d5b3
NayTmk <merkuriypo@gmail.com>
2025-10-14 00:43:37 +0300

    refactor(db): update async session setup

--- app/core/db.py
+++ app/core/db.py
@@ -2,7 +2,7 @@ from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-from app.config import settings
+from app.core.config import settings
 
 
 DATA_BASE_URL = settings.DATA_BASE_URL
