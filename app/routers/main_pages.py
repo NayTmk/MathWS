e3f8c1953776148617e0b943fc5c1ce194f6d361
NayTmk <merkuriypo@gmail.com>
2025-10-15 22:01:05 +0300

    refactor: change response data in main_page context

--- app/routers/main_pages.py
+++ app/routers/main_pages.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request
 
 from app.core.config import settings
+from app.domain.game_managers import task_manager
 from app.utils.deps import CurrentUser, SessionDep
 
 router = APIRouter()
@@ -10,8 +11,9 @@ router = APIRouter()
 async def main_page(
         request: Request, user: CurrentUser, session: SessionDep
 ):
+    operations = task_manager.get_operators_with_desc()
     return settings.TEMPLATES.TemplateResponse(
         request=request,
         name='main_page.html',
-        context={'user': user, 'operations': {'a': 'b'}}
+        context={'user': user, 'operations': operations}
     )
